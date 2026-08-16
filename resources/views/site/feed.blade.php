12a4956938cc45147e66011fb800c32856a0e190
Carlos Dagorret <dagorret@gmail.com>
2026-08-16 02:25:48 -0300

    Convert feed from Atom to RSS format

--- resources/views/site/feed.blade.php
+++ resources/views/site/feed.blade.php
@@ -1,18 +1,21 @@
 {{ '<?xml version="1.0" encoding="utf-8"?>' }}
-<feed xmlns="http://www.w3.org/2005/Atom">
-    <title>Bitácora de Ensayos</title>
-    <link href="{{ $baseUrl }}/feed.xml" rel="self"/>
-    <link href="{{ $baseUrl }}/"/>
-    <updated>{{ now()->toAtomString() }}</updated>
-    <id>{{ $baseUrl }}/</id>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>Dagorret</title>
+    <link>https://dagorret.com.ar/</link>
+    <description>Bitácora de ensayos sobre sistemas, filosofía y tecnología.</description>
+    <atom:link href="https://dagorret.com.ar/feed.xml" rel="self" type="application/rss+xml" />
 
     @foreach($posts as $post)
-    <entry>
-        <title><![CDATA[{{ $post->title }}]]></title>
-        <link href="{{ $baseUrl }}/{{ $post->slug }}/"/>
-        <id>{{ $baseUrl }}/{{ $post->slug }}/</id>
-        <updated>{{ $post->updated_at->toAtomString() }}</updated>
-        <summary><![CDATA[{{ $post->keywords }}]]></summary>
-    </entry>
+    <item>
+      <title><![CDATA[{{ $post->title }}]]></title>
+      <link>https://dagorret.com.ar/{{ $post->slug }}/</link>
+      <guid>https://dagorret.com.ar/{{ $post->slug }}/</guid>
+      <pubDate>{{ $post->updated_at->toRfc2822String() }}</pubDate>
+      @if(isset($post->category))
+      <category>{{ $post->category }}</category>
+      @endif
+    </item>
     @endforeach
-</feed>
+  </channel>
+</rss>
